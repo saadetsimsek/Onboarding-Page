b6895a1801ad245bb7850d067a348665b1de806e
saadetsimsek <saadetsimsek13@gmail.com>
2025-03-10 23:16:26 +0300

    Initial Commit

---
+++ OnboardingPage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnboardingPage
+//
+//  Created by Saadet Şimşek on 10/03/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ OnboardingPageTests/OnboardingPageTests.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingPageTests.swift
+//  OnboardingPageTests
+//
+//  Created by Saadet Şimşek on 10/03/2025.
+//
+
+import Testing
+@testable import OnboardingPage
+
+struct OnboardingPageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
